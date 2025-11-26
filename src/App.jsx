4bad662216b8ed975750179cdd3c158edaeff624
pedroinfante92 <166009636+pedroinfante92@users.noreply.github.com>
2025-11-26 13:55:31 +0100

    feat: add NavBar with links for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/favorites" element={<Favorites />}/>
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
