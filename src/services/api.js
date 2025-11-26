78387ae5d47b3ea968fe19c09ea3320b1c6aa9a1
pedroinfante92 <166009636+pedroinfante92@users.noreply.github.com>
2025-11-26 16:07:51 +0100

    feat: load movies from API on initial render using useEffect

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ const API_KEY = "3f49cf40e1df9d41ae58d19e9a6e9613";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movies/popular?api_key=${API_KEY}`);
-  const data = response.json();
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
   return data.results;
 };
 
@@ -11,9 +11,6 @@ export const searchMovies = async (query) => {
   const response = await fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
   );
-  const data = response.json();
+  const data = await response.json();
   return data.results;
 };
-
-console.log(getPopularMovies())
-console.log(searchMovies("Harry Potter"))
