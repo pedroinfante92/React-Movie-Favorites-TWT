e778d2ab623ae79ecffdc9c8a9c8ffeb22d0edd7
pedroinfante92 <166009636+pedroinfante92@users.noreply.github.com>
2025-11-26 17:56:31 +0100

    fix: gitignore update

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
-const API_KEY = "3f49cf40e1df9d41ae58d19e9a6e9613";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
+    `${API_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
   );
   const data = await response.json();
   return data.results;
